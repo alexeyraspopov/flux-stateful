40849b40bcb44a77772fe0b48e6232a43e8125fc
Alexey Raspopov <alexey.raspopov@datarobot.com>
2015-03-28 23:33:01 +0200

    naive implementation

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-module.exports = function(dispatcher, store){
-	// assign({}, )
+var EventEmitter = require('events').EventEmitter,
+	assign = require('object-assign');
 
-	return dispatcher.register(function(action){
-		if(store.hasOwnProperty(action.type)){
-			store[action.type](action);
-		}
-	});
+module.exports = function(dispatcher, methods){
+	var store = assign({
+		state: {},
+
+		setState: function(patch){
+			assign(this.state, patch);
+			this.emit('change');
+		},
+
+		getStoreData: function(){
+			return this.state;
+		},
+
+		dispatchToken: dispatcher.register(function(action){
+			if(methods.hasOwnProperty(action.type)){
+				store[action.type](action);
+			}
+		})
+	}, EventEmitter.prototype, methods);
+
+	return store;
 };

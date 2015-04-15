0a9841db5da934397b2732cf9014b835ce42237c
Alexey Raspopov <alexey.raspopov@datarobot.com>
2015-04-15 18:14:23 -0400

    browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,25 @@
 var gulp = require('gulp'),
-	jasmine = require('gulp-jasmine');
+	jasmine = require('gulp-jasmine'),
+	browserify = require('gulp-browserify'),
+	file = require('gulp-file');
 
 gulp.task('specs', function(){
 	return gulp.src('specs.js')
 		.pipe(jasmine());
 });
 
-gulp.task('default', ['specs']);
+gulp.task('browserify', ['browserify-simple', 'browserify-pure']);
+
+gulp.task('browserify-simple', function() {
+	return file('index.browser.js', 'window.Store = require("./index");', { src: true })
+		.pipe(browserify())
+		.pipe(gulp.dest('./dist'));
+});
+
+gulp.task('browserify-pure', function() {
+	return file('immutable.browser.js', 'window.ImmutableStore = require("./immutable");', { src: true })
+		.pipe(browserify())
+		.pipe(gulp.dest('./dist'));
+})
+
+gulp.task('default', ['specs', 'browserify']);

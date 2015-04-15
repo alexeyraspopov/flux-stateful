c990eb9ef99e03e9cc857e0175aa1e12ff2435f7
Alexey Raspopov <alexey.raspopov@datarobot.com>
2015-04-15 18:17:11 -0400

    complexity

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,17 @@
 var gulp = require('gulp'),
 	jasmine = require('gulp-jasmine'),
 	browserify = require('gulp-browserify'),
+	complexity = require('gulp-complexity'),
 	file = require('gulp-file');
 
 gulp.task('specs', function(){
 	return gulp.src('specs.js')
 		.pipe(jasmine());
+});
+
+gulp.task('complexity', function() {
+	return gulp.src(['index.js', 'immutable.js', 'src/*.js'])
+		.pipe(complexity());
 });
 
 gulp.task('browserify', ['browserify-simple', 'browserify-pure']);
@@ -20,6 +26,6 @@ gulp.task('browserify-pure', function() {
 	return file('immutable.browser.js', 'window.ImmutableStore = require("./immutable");', { src: true })
 		.pipe(browserify())
 		.pipe(gulp.dest('./dist'));
-})
+});
 
-gulp.task('default', ['specs', 'browserify']);
+gulp.task('default', ['specs', 'complexity', 'browserify']);

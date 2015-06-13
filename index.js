24b265bee049177764876d18e6f4796e7fb9774e
Alexey Raspopov <alexey.raspopov@datarobot.com>
2015-06-13 10:48:16 +0300

    removed setState

--- index.js
+++ index.js
@@ -1,12 +1,2 @@
-var assign = require('object-assign'),
-	store = require('./src/store');
-
-module.exports = store.bind(null, {
-	dispatch: function(actionType, payload) {
-		this[actionType](payload);
-	},
-	setState: function(patch){
-		assign(this.state, patch);
-		this.publish(this.serialize(this.state));
-	}
-});
+exports.Immutable = require('./immutable');
+exports.Mutable = require('./mutable');

3873b99ba043f930e00815c6caf5a7339a65606d
Alexey Raspopov <alexey.raspopov@datarobot.com>
2015-06-14 12:38:57 -0400

    remove everything I don't need [2]

--- index.js
+++ index.js
@@ -1,4 +1,57 @@
-var createStore = require('./src/store');
+var assign = require('object-assign'),
+	newsletter = require('newsletter');
+
+function getInitialState(methods) {
+	return typeof methods.getInitialState === 'function' ? methods.getInitialState() : {};
+}
+
+function identity(a) {
+	return a;
+}
+
+function mutate(store, type, action) {
+	switch (type) {
+	case 'mutable':
+		store[action.type](store.state, action);
+		break;
+	case 'immutable':
+		store.state = store[action.type](store.state, action);
+		break;
+	}
+}
+
+function createStore(storeType) {
+	return function(dispatcher, methods) {
+		var store, token;
+
+		function dispatchAction(action) {
+			if(typeof methods[action.type] === 'function'){
+				mutate(store, storeType, action);
+				store.publish(store.serialize(store.state));
+			}
+		}
+
+		function serializeState() {
+			if(dispatcher.isDispatching()){
+				dispatcher.waitFor([token]);
+			}
+
+			return store.serialize(store.state);
+		}
+
+		store = assign({
+			state: getInitialState(methods),
+			getState: serializeState,
+			serialize: identity,
+			dispatchToken: token
+		}, newsletter(), methods);
+
+		token = dispatcher.register(dispatchAction);
+
+		return store;
+	};
+};
+
 
 exports.Immutable = createStore('immutable');
 exports.Mutable = createStore('mutable');
